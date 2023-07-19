ad365b9ebb254098f9b4cf5c616f1b2bca0b2179
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-07-19 13:47:48 +0100

    sum of two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * add - adds two values
- * @n1: first number to be computed 
- * @n2: second number to be computed
- * Return: integer value
+* add - adds two values
+* @n1: first number to be computed
+* @n2: second number to be computed
+* Return: integer value
 */
 
-int add(int n1, int n2)
+intadd(int n1, int n2)
 {
-	return (n1 + n2);
+return (n1 + n2);
 }
