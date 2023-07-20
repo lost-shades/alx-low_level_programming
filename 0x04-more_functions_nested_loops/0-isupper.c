20df464ae70ebd7244b69c36cb032b1fc5d3ebad
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-07-21 00:38:59 +0100

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character.
+ * @c: character
+ * Return: 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if (c <= '90' && c >= '65')
+	{
+		return (1);
+	}
+	return (0);
+}
