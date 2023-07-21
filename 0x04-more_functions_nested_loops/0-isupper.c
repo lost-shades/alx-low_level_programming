937f4d64ffcaff9b052b729f9673498b4e3edbd7
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-07-21 01:11:48 +0100

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,11 +5,13 @@
  * @c: character
  * Return: 1 if c is uppercase and 0 otherwise.
  */
+
 int _isupper(int c)
+
 {
-	if (c <= '90' && c >= '65')
+	if (c >= 65 && c <= 90)
 	{
-		return (1);
+	return (1);
 	}
 	return (0);
 }
