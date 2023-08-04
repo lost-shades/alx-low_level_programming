1a355d1c427119a39fb30c41b121bdcd03b7922c
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-05 00:43:15 +0100

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - function to print no of argument
+ * @argc: argument count
+ * @argv: argument arrays
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
