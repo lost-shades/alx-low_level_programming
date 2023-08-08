3c68a920fd65986e8eeaaf325cdccaa4f86e39e0
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-08 15:22:49 +0100

    second task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * _strdup - function that returns a ptr to a newly allocated space in memory
+ * @str: the string given as parameter
+ * Return: ptr to duplicate string or NULL if memory is insufficient
+*/
+
+char *_strdup(char *str)
+{
+	char *new_str;
+	unsigned int i;
+	unsigned int newstr_len = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	new_str = malloc(sizeof(char) * (newstr_len + 1));
+	if (new_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < newstr_len + 1; i++)
+	{
+		new_str[i] = str[i];
+	}
+	return (new_str);
+}
