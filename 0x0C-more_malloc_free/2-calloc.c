83d5163746834daee62a8d25b1cd8941b7c422c7
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-10 18:52:55 +0100

    task 2 calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,7 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *array;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,6 +20,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (array == NULL)
 	{
 		return (NULL);
+	}
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		array[i] = 0;
 	}
 	return (array);
 }
