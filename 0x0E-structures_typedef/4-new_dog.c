d2f8da46925747b4a34e7e28ceb0b88c61f74127
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-15 07:01:06 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,14 +7,16 @@
  * @age: age of dog
  * @owner: owner of dog
  *
- * Return: NULL if fails.
+ * Return: ptr to new dog function or NULL if function fails.
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
-	char *a, *b;
+	char *cpy_name, *cpy_owner;
 	int i = 0, j = 0, k, l;
+
+	/* i=length of dog name while j=length of dog owner */
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -29,25 +31,25 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	a = malloc(sizeof(char) * i + 1);
-	if (a == NULL)
+	cpy_name = malloc(sizeof(char) * i + 1);
+	if (cpy_name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	b = malloc(sizeof(char) * j + 1);
-	if (b == NULL)
+	cpy_owner = malloc(sizeof(char) * j + 1);
+	if (cpy_owner == NULL)
 	{
 		free(new_dog);
-		free(a);
+		free(cpy_name);
 		return (NULL);
 	}
 	for (k = 0; k <= i; k++)
-		a[k] = name[k];
+		cpy_name[k] = name[k];
 	for (l = 0; l <= j; l++)
-		b[l] = owner[l];
-	new_dog->name = a;
+		cpy_owner[l] = owner[l];
+	new_dog->name = cpy_name;
 	new_dog->age = age;
-	new_dog->owner = b;
+	new_dog->owner = cpy_owner;
 	return (new_dog);
 }
