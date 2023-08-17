3a01f4d65b53f1c8c2288035c278bf6c28103b58
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-17 22:45:28 +0100

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		const char *x = va_arg(str, const char *);
 
 		if (str == NULL)
-                        printf("nil");
+			printf("nil");
 		else
 			printf("%s", x);
 
