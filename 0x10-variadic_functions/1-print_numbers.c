557d24a74decb578d6f92b415ab8e96934dc9260
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-17 22:10:49 +0100

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%d", x);
 
-		if (separator != NULL && i < n -1)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 	}
 	va_end(args);
