38565763b76c0607f9f5b22a61b2227bae1429d9
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-08-24 13:27:09 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,7 +4,7 @@
  * print_list - prints all the elements of a list.
  * @h: pointer to the head of node.
  *
- * Return: size of elements in list.
+ * Return: number of elements in list.
  */
 
 size_t print_list(const list_t *h)
