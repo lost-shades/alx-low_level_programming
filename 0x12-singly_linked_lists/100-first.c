b65f1379a7a60bd24b93a9c9be95ff4d09ab34e3
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-25 20:35:52 +0100

    before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_pre_main_message - function that prints a specific before the main is executed
+ */
+
+void print_pre_main_message(void) 
+{
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
+
+/*
+ * __attribute__((constructor)) is a GCC-specific attribute that specifies that 
+ * the before_main function should be executed before the main function.
+ */
+
+__attribute__((constructor))
+void before_main(void) 
+{
+    print_pre_main_message();
+}
+
