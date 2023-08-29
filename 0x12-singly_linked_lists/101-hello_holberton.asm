93cf22433b6ce6925c44a4dc3e7458b31c9daf9b
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-08-29 09:21:53 +0100

    hello holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,18 @@
 section .data
-	hello db "Hello, Holberton", 10
+	hello db "Hello, Holberton", 0
 	format db "%s", 0
 
 section .text
 	global main
-
-extern printf
+	extern printf
 
 main:
 	push rbp
 	mov rdi, format
 	mov rsi, hello
+	xor rax, rax
 	call printf
-
 	pop rbp
+
+	xor rax, rax
 	ret
