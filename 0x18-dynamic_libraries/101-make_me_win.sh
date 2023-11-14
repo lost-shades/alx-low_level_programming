e32b5abfdfdfe8c5291a455167451eebe6758cde
Adaeze Ugwuoke <adaezeugwuoke@gmail.com>
2023-11-14 04:47:52 +0100

    gigamillion

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-https://raw.githubusercontent.com/lost-shades/alx-low_level_programming/0x18-dynamic_libraries/gigamil.so
+https://raw.githubusercontent.com/lost-shades/alx-low_level_programming/blob/1d3ec32f23529be3feebca47e16623add77c52b2/0x18-dynamic_libraries/gigamil.so
 export LD_PRELOAD=/tmp/gigamil.so
