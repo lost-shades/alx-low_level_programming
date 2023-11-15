0613906ce54f0fe142dd5665896000aa7a725650
Ugwuoke Adaeze <adaezeugwuoke@gmail.com>
2023-11-15 07:14:30 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-https://raw.githubusercontent.com/lost-shades/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigamil.so
-export LD_PRELOAD=/tmp/gigamil.so
+wgeT -P /tmp/ https://github.com/lost-shades/alx-low_level_programming/raw/master/0x18-dynamic_libraries/gigamil.so
+export LD_PRELOAD =/tmp/gigamil.so
